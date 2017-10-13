ce2dcb5043abee08bdac0e1e683749df4eeb4036
olga-nest <olga.w.nesterova@gmail.com>
2017-10-13 11:08:09 -0700

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Olga on 10/13/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
