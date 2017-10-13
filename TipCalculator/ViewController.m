740701bc315b09734ed24e199cf9688688de564b
olga-nest <olga.w.nesterova@gmail.com>
2017-10-13 11:42:54 -0700

    Get user's input

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -10,19 +10,39 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
+@property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+
+@property (nonatomic, assign) int billAmountInDollars;
+@property (nonatomic, assign) int tipAmountInDollars;
+@property (nonatomic) NSString *userInput;
+
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.billAmountInDollars = 0.0;
+    
+    
+    
+    
 }
 
+- (IBAction)calculateTip:(UIButton *)sender {
+    // Get the user's input
+    //TODO: Move to separate function
+    NSString *usersInput;
+    usersInput = self.billAmountTextField.text;
+    NSLog(@"user's input: %@", usersInput);
+    self.billAmountInDollars = [usersInput intValue];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    //Calculate Tip Amount
+    self.tipAmountInDollars = self.billAmountInDollars + 15;
+
+    NSLog(@"tip amount in $: %d", self.tipAmountInDollars);
 }
 
 
