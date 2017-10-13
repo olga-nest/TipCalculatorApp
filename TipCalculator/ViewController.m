eb20ca2773e558c2bd5f701b257f70af2d395c93
olga-nest <olga.w.nesterova@gmail.com>
2017-10-13 12:41:47 -0700

    Add tip % field, change keybord type to phone pad, add x on text fields

--- TipCalculator/ViewController.m
+++ TipCalculator/ViewController.m
@@ -12,12 +12,14 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *billAmountTextField;
 @property (weak, nonatomic) IBOutlet UILabel *tipAmountLabel;
+@property (weak, nonatomic) IBOutlet UITextField *tipAmountTextField;
 
 @property (nonatomic, assign) float billAmountInDollars;
 @property (nonatomic, assign) float tipAmountInDollars;
-@property (nonatomic) NSString *userInput;
+@property (nonatomic) NSString *userInputAmount;
 @property (nonatomic) NSString *tipAmountString;
-
+@property (nonatomic) NSString *usersInputTip;
+@property (nonatomic, assign) int tipPercentage;
 
 @end
 
@@ -25,21 +27,26 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self setupTextField];
     self.billAmountInDollars = 0.0;
-    
-    
 }
 
 - (IBAction)calculateTip:(UIButton *)sender {
-    // Get the user's input
+    // Get the user's Amount input
     //TODO: Move to separate function
-    NSString *usersInput;
-    usersInput = self.billAmountTextField.text;
-    NSLog(@"user's input: %@", usersInput);
-    self.billAmountInDollars = [usersInput intValue];
+    NSString *usersInputAmount;
+    usersInputAmount = self.billAmountTextField.text;
+    NSLog(@"user's Amount input: %@", usersInputAmount);
+    self.billAmountInDollars = [usersInputAmount intValue];
 
+    //Get the user's Tip % input
+    NSString *usersInputTip;
+    usersInputTip = self.tipAmountTextField.text;
+    NSLog(@"user's Tip input: %@", usersInputTip);
+    self.tipPercentage = [usersInputTip intValue];
+    
     //Calculate Tip Amount
-    self.tipAmountInDollars = (self.billAmountInDollars * 15)/100;
+    self.tipAmountInDollars = (self.billAmountInDollars * self.tipPercentage)/100;
 
     NSLog(@"tip amount in $: %f", self.tipAmountInDollars);
    
@@ -47,7 +54,19 @@
     NSLog(@"tip string: %@ ", tipAmountString);
     [self.tipAmountLabel setText:tipAmountString];
     
-    NSLog(@"new label text: %@ ", self.tipAmountLabel.text);
+    [self.billAmountTextField setText:@""];
+    [self.tipAmountTextField setText:@""];
+}
+
+- (void)setupTextField
+{
+    // set the keyboard type to phonePad
+    self.billAmountTextField.keyboardType = UIKeyboardTypePhonePad;
+    self.billAmountTextField.clearButtonMode = UITextFieldViewModeUnlessEditing;
+    
+    self.tipAmountTextField.keyboardType = UIKeyboardTypePhonePad;
+    self.tipAmountTextField.clearButtonMode = UITextFieldViewModeUnlessEditing;
+
 }
 
 
